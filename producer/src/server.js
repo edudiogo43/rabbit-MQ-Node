b121fd0aaddfcf11811dd46c02044f57b9bc6688
Eduardo Diogo Garcia <Eduardo.Diogo.Garcia@kyndryl.com>
2022-04-26 11:40:22 -0300

    generate random coin

--- producer/src/server.js
+++ producer/src/server.js
@@ -6,10 +6,18 @@ const generateBitCoinsData = async () => {
 
     const messageChannel = await createMessageChannel();
 
+    const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","W","Y","Z"];
+
+    const coinRand = letters[Math.floor(Math.random() * letters.length)]+
+                     letters[Math.floor(Math.random() * letters.length)]+
+                     letters[Math.floor(Math.random() * letters.length)];
+
+    const coinValue = (Math.random(1000) * 5000).toFixed(2);
+
     if (messageChannel) {
         let bitCoins = [];
         bitCoins.push({
-            symbol: 'BTC', value: Math.random(1000) * 5000
+            symbol: coinRand, value: coinValue
         });
 
         const name = bitCoins[0].symbol;
